5b13e6a1470eeb9305861209ae3d857a003c60c0
omseervi098 <73896710+omseervi098@users.noreply.github.com>
2026-07-03 22:40:32 +0530

    changed command name to linman

--- apphub/main.py
+++ apphub/main.py
@@ -5,7 +5,7 @@ from apphub.core.logger import get_logger
 
 logger = get_logger("root")
 app = typer.Typer(
-    no_args_is_help=True, help="AppHub Terminal — manage Linux apps across all formats."
+    no_args_is_help=True, help="Linux App Manager — manage Linux apps across all formats."
 )
 
 app.add_typer(cli_app, name="")  # mount subcommands at root level
